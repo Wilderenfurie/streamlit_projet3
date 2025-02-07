055978fcd26ff0bd6850a97ffa2e1a5e9f11b157
Wilderenfurie <jbiencou@gmail.com>
2025-02-07 12:01:39 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import sqlalchemy
+
+
+# Initialize connection.
+#conn = st.connection("postgresql", type="sql")
+
+# Perform query.
+#df = conn.query('SELECT * FROM yahoo;', ttl="10m")
+
+# Print results.
+#for row in df.itertuples():
+#    st.write(f"{row.name} has a :{row.pet}:")
