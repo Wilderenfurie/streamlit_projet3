d3db672aba443174375fb281c5212a2905689e29
Wilderenfurie <jbiencou@gmail.com>
2025-02-10 14:34:39 +0100

    connect postgress

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import sqlalchemy
 
 
 # Initialize connection.
-#conn = st.connection("postgresql", type="sql")
+conn = st.connection("postgresql", type="sql")
 
 # Perform query.
-#df = conn.query('SELECT * FROM yahoo;', ttl="10m")
+df = conn.query("""SELECT * FROM yahoo;""", ttl="10m")
 
 # Print results.
-#for row in df.itertuples():
-#    st.write(f"{row.name} has a :{row.pet}:")
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
